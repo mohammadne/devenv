60145f3aaf1f60745e5d70e93c52d46798a9b5b7
mohammadne <mohammadne.dev@gmail.com>
2023-10-27 16:01:20 +0000

    Add downloader

--- scripts/library/downloader.sh
+++ scripts/library/downloader.sh
@@ -10,3 +10,14 @@ function clone_if_not_exists() {
 
     git clone "$1" "$2" "${@:3}" -q
 }
+
+# parameter 1: url - string
+# parameter 2: destination directory - string
+# parameter 3: clone options
+function curl_if_not_exists() {
+    if [ -d "$2" ] ; then
+        return
+    fi
+
+    curl "$1" -o "$2" "${@:3}" --silent
+}
--- scripts/modules/vim.sh
+++ scripts/modules/vim.sh
@@ -9,6 +9,6 @@ run() {
 
     linker $1 "$dotfiles_directory/vim/vimrc" "$HOME/.vimrc"
 
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl_if_not_exists "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" "$HOME/.vim/autoload/plug.vim" -fL --create-dirs
     vim -E +'PlugInstall --sync' +qall > /dev/null || print_error $1 "Something went wrong installing vim plugins."
 }
