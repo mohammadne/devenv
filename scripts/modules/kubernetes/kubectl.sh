e69a4648d75a6e184ec2531a6537e426cc8bfd21
mohammadne <mohammadne.dev@gmail.com>
2023-10-27 17:49:36 +0000

    Add kubectl todo

--- scripts/modules/kubernetes/kubectl.sh
+++ scripts/modules/kubernetes/kubectl.sh
@@ -15,4 +15,6 @@ run() {
     local latest_version=$(curl -L -s https://dl.k8s.io/release/stable.txt)
     curl_if_not_exists "https://dl.k8s.io/release/$latest_version/bin/linux/amd64/kubectl" "/tmp" -LO
     sudo mv /tmp/kubectl "$destination" && chmod +x "$destination/kubectl"
+
+    # TODO: install plugins (kubectl-neat, kubectl-ns)
 }
