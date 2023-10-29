2b93e6237750d73e4da8431ccf2cd46092ef4714
mohammadne <mohammadne.dev@gmail.com>
2023-10-29 17:18:04 +0000

    Change docker version

--- scripts/modules/docker/install-binary.sh
+++ scripts/modules/docker/install-binary.sh
@@ -5,11 +5,11 @@ info() {
 }
 
 run() {
-    local version="docker-17.06.2-ce"
+    local version="docker-24.0.4"
     local architecture=$(system_architecture)
     local destination="/tmp/$version.tgz"
 
-    curl_if_not_exists "https://download.docker.com/linux/static/stable/$architecture/$version.tgz" "$destination" -sSL -O
+    curl_if_not_exists "https://download.docker.com/linux/static/stable/$architecture/$version.tgz" "$destination" -SL
     cd /tmp && tar zxf "$destination"
     sudo mv "/tmp/docker/docker" "/usr/local/bin"
     sudo chmod +x /usr/local/bin/docker
