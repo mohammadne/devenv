262cf782c56b99c515fd63ab338336676f8f6ca9
mohammadne <mohammadne.dev@gmail.com>
2023-10-29 15:51:07 +0000

    enhance files

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,48 +9,37 @@ for library_file in "$scripts_directory/library"/*; do source "$library_file"; d
 
 source_values "$scripts_directory/values.env"
 
-# print_message "dotfiles" "ensure dotfiles repository is present with the latest changes"
-# ensure_dotfiles "dotfiles"
+print_message "dotfiles" "ensure dotfiles repository is present with the latest changes"
+ensure_dotfiles "dotfiles"
 
-function help() {
+function help_command() {
     echo "HELP"
     exit 0
 }
 
-function info() {
-    echo "INFO"
-    exit 0
-}
-
-function set() {
+function set_command() {
     # at least one argument should be passed
     if [ $# -ne 1 ]; then
-        print_error "main" "invalid set arguments"
+        print_error "set" "invalid number of arguments"
         help;
     fi
 
     source_values $1
 }
 
-function install() {
-    echo "INSTALL"
-    exit 0
-}
-
 # at least one argument should be passed
-if [ $# -eq 0 ]; then help; fi
+if [ $# -eq 0 ]; then help_command; fi
 
 # Parse arguments 
 while [ $# -gt 0 ]; do
     case $1 in
-        --help) help ;;
-        --info) info ;;
-        --set) set $2; shift 2 ;;
-        *) install_list+=("$1"); shift ;;
+        --help) help_command ;;
+        --set) set_command $2; shift 2 ;;
+        *) modules_list+=("$1"); shift ;;
     esac
 done
 
-for module in "${install_list[@]}"; do
+for module in "${modules_list[@]}"; do
     echo # a newline to seperate modules from each others
 
     source "$scripts_directory/modules/$module.sh" 2>/dev/null || {
