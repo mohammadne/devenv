29f71292f179fe7f26d45be79d4edd1f912423a5
mohammadne <mohammadne.dev@gmail.com>
2023-11-10 22:43:13 +0330

    Add non-interactive flag

--- scripts/core/requirements.sh
+++ scripts/core/requirements.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install packages non-interactively
+export DEBIAN_FRONTEND=noninteractive
+
 function _check_timezne() {
     local TZ=Iran
     ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
