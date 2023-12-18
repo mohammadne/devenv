ce8fedc49e1155245275020096dbe6687c38836d
mohammadne <mohammadne.dev@gmail.com>
2023-12-18 16:57:15 +0330

    feat: add vscode installation script

--- scripts/modules/docker/full-setup.sh
+++ scripts/modules/docker/full-setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 info() {
-	echo -n "install docker client and daemon"
+    echo -n "install docker client and daemon"
 }
 
 run() {
-    echo "implement me"
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    
+    sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    
+    sudo systemctl start docker
 }
