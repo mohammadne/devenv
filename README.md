94e0ac4c6572888101577fd344a9922f1dea8b91
mohammadne <mohammadne.dev@gmail.com>
2024-08-20 12:25:20 +0330

    feat: update cloner library

--- README.md
+++ README.md
@@ -12,8 +12,12 @@ Provisioner for setup a tailored environment for development
 > **_NOTE:_** I'm using Fedora as my desktop OS and Ubuntu as my development server choice and this provisioner repository (devenv) is written and tested based on them.
 
 ```sh
+# generate ssh-key for your github account and add it to your account
+ssh-keygen -t ed25519 -f ~/.ssh/github_ed25519 -N "" -C "" > /dev/null
+
 # specify your preferred location for cloning after '--' at the end of the script below
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/mohammadne/devenv/main/scripts/clone.sh)"
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/mohammadne/devenv/main/scripts/clone.sh)"
+git clone git@github.com:mohammadne/devenv.git Workspace/personal/devenv
 
 # change to your devenv directory and install required packages
 cd $HOME/Workspace/personal/devenv && ./scripts/install.sh git ssh
--- scripts/library/cloner.sh
+++ scripts/library/cloner.sh
@@ -4,46 +4,42 @@
 # parameter 2: destination directory - string
 # parameter 3: clone options
 function clone_if_not_exists() {
-    if [ -d "$2" ] ; then
-        return
-    fi
-
-    git clone "$1" "$2" "${@:3}" -q
+  if [ -d "$2" ] ; then
+    return
+  fi
+  
+  git clone "$1" "$2" "${@:3}" -q
 }
 
 # parameter 1: module - string
 # parameter 2: directory to be cloned - string
-# parameter 3: remote https url - string
-# parameter 4: remote ssh url - string
+# parameter 3: remote ssh url - string
 function full_clone() {
-    local module=$1
-    local directory=$2
-    local remote_https=$3
-    local remote_ssh=$4
-
-    # clone if not exist
-    if [ ! -d "$directory" ]; then
-        mkdir -p "$(dirname $directory)"
-        git clone $remote_https $directory -q
-        cd $directory && git remote set-url origin $remote_ssh
-        return
-    fi
-
-    if [ ! -d "$directory/.git" ]; then
-        print_error $module "directory is present but is not a git repository"
-        exit 1
-    fi
-
-    current_remote="$(cd $directory && git config --get remote.origin.url)"
-    if [ "$current_remote" != "$remote_ssh" ] && [ "$current_remote" != "$remote_https" ]; then
-        print_error $module "existing directory has an invalid remote url"
-        exit 1
-    fi
-
-    cd $directory && git remote set-url origin $remote_ssh
-    cd $directory && git pull origin -q --force
-    if [ $? -ne 0 ]; then
-        print_error $module "unable to pull the latest changes"
-        exit 1
-    fi
+  local module=$1
+  local directory=$2
+  local remote_url=$3
+  
+  # clone if not exist
+  if [ ! -d "$directory" ]; then
+    mkdir -p "$(dirname $directory)"
+    git clone $remote_url $directory -q
+    return
+  fi
+  
+  if [ ! -d "$directory/.git" ]; then
+    print_error $module "directory is present but is not a git repository"
+    exit 1
+  fi
+  
+  current_remote="$(cd $directory && git config --get remote.origin.url)"
+  if [ "$current_remote" != "$remote_url" ]; then
+    print_error $module "existing directory has an invalid remote url"
+    exit 1
+  fi
+  
+  cd $directory && git pull origin -q --force
+  if [ $? -ne 0 ]; then
+    print_error $module "unable to pull the latest changes"
+    exit 1
+  fi
 }
