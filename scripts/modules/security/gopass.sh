dda30540822f7aec1f8ea895a78538ec8a894a36
mohammadne <mohammadne.dev@gmail.com>
2024-11-26 03:08:14 -0500

    fix: update gopass script

--- scripts/modules/security/gopass.sh
+++ scripts/modules/security/gopass.sh
@@ -5,7 +5,7 @@ info() {
 }
 
 run() {
-  local version="v1.15.15"
+  local version="1.15.15"
   _install_gopass $1 $version
   _install_gopass_jsonapi $1 $version
   
@@ -15,18 +15,18 @@ run() {
 
 _install_gopass() {
   local binary_path="/usr/local/bin/gopass"
-  local url="https://github.com/gopasspw/gopass/releases/download/v${version}/helmsman_${version}_linux_amd64.tar.gz"
+  local url="https://github.com/gopasspw/gopass/releases/download/v${version}/gopass-${version}-linux-amd64.tar.gz"
   
   if check_versioned_binary $1 $binary_path $2 "-v"; then return; fi
   if ! result=$(download_file $1 $url); then echo "$result" && return 1; fi
-  sudo tar -C $(dirname $binary_path) -xzf $result "helmsman" && rm -rf "$result"
+  sudo tar -C $(dirname $binary_path) -xzf $result "gopass" && rm -rf "$result"
 }
 
 _install_gopass_jsonapi() {
   local binary_path="/usr/local/bin/gopass-jsonapi"
-  local url="https://github.com/gopasspw/gopass-jsonapi/releases/download/v${version}/helmsman_${version}_linux_amd64.tar.gz"
+  local url="https://github.com/gopasspw/gopass-jsonapi/releases/download/v${version}/gopass-jsonapi-${version}-linux-amd64.tar.gz"
   
   if check_versioned_binary $1 $binary_path $2 "-v"; then return; fi
   if ! result=$(download_file $1 $url); then echo "$result" && return 1; fi
-  sudo tar -C $(dirname $binary_path) -xzf $result "helmsman" && rm -rf "$result"
+  sudo tar -C $(dirname $binary_path) -xzf $result "gopass-jsonapi" && rm -rf "$result"
 }
