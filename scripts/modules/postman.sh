9dac5e71a0e02c0936adf79153c1004fb56576e9
mohammadne <mohammadne.dev@gmail.com>
2025-03-12 21:02:32 +0330

    chore: add postman downloader

--- scripts/modules/postman.sh
+++ scripts/modules/postman.sh
@@ -6,16 +6,24 @@ info() {
 
 run() {
   sudo dnf install openssl -yq
-  
-  # curl the zip
-}
 
-# File: /home/mohammadne/.local/share/applications/postman.desktop
-# [Desktop Entry]
-# Encoding=UTF-8
-# Name=Postman
-# Exec=postman
-# Icon=/home/mohammadne/Programs/Postman/app/icons/icon_128x128.png
-# Terminal=false
-# Type=Application
-# Categories=Development;
+  local applications_path="$HOME/Applications"
+  local url="https://dl.pstmn.io/download/latest/linux_64"
+
+  if ! result=$(download_file $1 $url); then echo "$result" && return 1; fi
+  tar -C $applications_path -xzf $result && rm -rf "$result"
+
+  local desktop_entry_file="$HOME/.local/share/applications/postman.desktop"
+  if [ ! -f "$desktop_entry_file" ]; then
+    cat <<EOF > "$desktop_entry_file"
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=$applications_path/Postman/Postman
+Icon=$applications_path/Postman/app/icons/icon_128x128.png
+Terminal=false
+Type=Application
+Categories=Development;
+EOF
+  fi
+}
